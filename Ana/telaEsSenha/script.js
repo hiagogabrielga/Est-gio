73b9a044f6e50cdb6f759b2fb700c3fa5de0791c
anacaroline16.github.io <caroline.cordeiro@estudante.ifro.edu.br>
2024-11-01 20:12:45 -0300

    Terminar

---
+++ Ana/telaEsSenha/script.js
@@ -0,0 +1,11 @@
+const senha = document.querySelector('.senha');
+const cf_senha = document.querySelector('.cf_senha');
+const caixa_pai = document.querySelector('.caixaPai');
+
+function caixa_pai (){
+    const caixa_pai = document.querySelector('.caixaPai');
+    // const h1 = document.querySelector('h1');
+    caixa_pai.style.backgroundcolor = '#A1FF7E';
+    // h1.style.backgroundcolor = 'white'
+}
+caixa_pai();
